3f491169348d8c86cb3c37af18417bb78ac4e6de
fennecdjay <astor.jeremie@wanadoo.fr>
2017-02-04 23:29:34 +0100

    .travis.yml.

--- util/linux_prepare.sh
+++ util/linux_prepare.sh
@@ -9,7 +9,8 @@ then
     sed -i 's/#USE_DOUBLE/USE_DOUBLE/' config.def.mk
 fi
 make
-sudo make install
+export LIBARY_PATH=$PWD
+#sudo make install
 popd
 
 if [ "$GW_FLOAT_TYPE" = "double" ]
@@ -23,8 +24,10 @@ sed -i "s/ALSA_D/#ALSA_D/" config.def.mk
 
 #sudo mkdir -p /usr/lib/Gwion/{doc,api,plug,tags,tok}
 #sudo chmod -R a+rw /usr/lib/Gwion
-export GWION_DOC_DIR="./doc"
-export GWION_API_DIR="./api"
-export GWION_TOK_DIR="./tok"
-export GWION_TAG_DIR="./tag"
-export GWION_PLUG_DIR="./plug"
+
+# use local directories as default location is not writable in Travis
+#export GWION_DOC_DIR="./doc"
+#export GWION_API_DIR="./api"
+#export GWION_TOK_DIR="./tok"
+#export GWION_TAG_DIR="./tag"
+#export GWION_PLUG_DIR="./plug"
