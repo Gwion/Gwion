1dd713fd4003f26502318d1a07d17202c437f3fe
fennecdjay <astor.jeremie@wanadoo.fr>
2017-02-05 18:09:55 +0100

    .travis.yml.

--- util/osx_prepare.sh
+++ util/osx_prepare.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #brew unlink bison
-#brew install valgrind libsndfile bison
-brew install valgrind libsndfile
-
+brew install valgrind libsndfile bison
+#brew install valgrind libsndfile
+brew link bison
 
 pushd Soundpipe
 if [ "$GW_FLOAT_TYPE" = "double" ]
@@ -27,8 +27,9 @@ sed -i '' "s/CFLAGS+=-DD_FUNC=alsa_driver/CFLAGS+=-DD_FUNC=dummy_driver/" config
 sed -i '' "s/ALSA_D/#ALSA_D/"                                             config.def.mk
 sed -i '' "s/-lsoundpipe/Soundpipe\/libsoundpipe.a/" Makefile
 sed -i '' "s/YACC ?= bison/YACC=$PWD/bison-3.0.4/src/bison"
-#sed -i '' "s/-o parser.c -dv gwion.y -x/-o parser.c -dv gwion.y/" ast/Makefile
+sed -i '' "s/-o parser.c -dv gwion.y -x/-o parser.c -dv gwion.y/" ast/Makefile
 sed -i '' 's/${YACC}/..\/bison-3.0.4\/src\/bison/' ast/Makefile
+cp -r data share
 #export doc dirs
 #export GWION_DOC_DIR="./doc"
 #export GWION_API_DIR="./api"
