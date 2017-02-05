bfa8ea7faf1746fb01625d2d6c1ab213ee3b8fde
fennecdjay <astor.jeremie@wanadoo.fr>
2017-02-05 13:41:34 +0100

    .travis.yml.

--- util/osx_prepare.sh
+++ util/osx_prepare.sh
@@ -33,3 +33,6 @@ export GWION_API_DIR="./api"
 export GWION_TOK_DIR="./tok"
 export GWION_TAG_DIR="./tag"
 export GWION_PLUG_DIR="./plug"
+
+echo 'export PATH="/usr/local/opt/bison/bin:$PATH"' >> ~/.bash_profile
+echo 'export LDFLAGS:  -L/usr/local/opt/bison/lib' >> ~/.bash_profile
