148767e65a1fbf5ebb28c6e4c7639cbb31ebb7f2
fennecdjay <astor.jeremie@wanadoo.fr>
2017-03-02 00:40:12 +0100

    Update.

--- util/coverage.sh
+++ util/coverage.sh
@@ -242,5 +242,25 @@ run() {
 	place_files
 }
 
+after_success() {
+	for a in tests/bug/*; do ./gwion $a; done;
+	bash util/coverage.sh run
+	#./gwion examples/doc.gw
+    EXCLUDE_TARGET=" eval/parser.c eval/lexer.c"
+    EXCLUDE_TARGET+=" $BISON_VERSION"
+    EXCLUDE_TARGET+=" Soundpipe"
+    EXCLUDE_TARGET+=" bison bats lua_install "
+    EXCLUDE_TARGET+=" examples coverage"
+    EXCLUDE_TARGET+=" drvr"
+    EXCLUDE_TARGET+=lua_install
+
+	local EXCLUDE
+    for exclude in $EXCLUDE_TARGET
+	do
+		EXCLUDE+=" --exclude $exclude"
+	done
+	coveralls "$EXCLUDE" --gcov-options '\-lp'
+}
+
 [ -z "$1" ] && usage
 "$1" "$2"
