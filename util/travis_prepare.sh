c54228c43d9446bec2856e82fdc8cf5d2538b6ab
fennecdjay <astor.jeremie@wanadoo.fr>
2017-03-02 13:12:21 +0100

    Fix typo.

--- util/travis_prepare.sh
+++ util/travis_prepare.sh
@@ -44,7 +44,7 @@ install_soundpipe() {
 	[ "$USE_DOUBLE" -eq 1 ] && export SP_FLOAT="double"
 	[ "$USE_DOUBLE" -eq 0 ] && export SP_FLOAT="float"
 	[ -z "$USE_DOUBLE" ] && export SP_FLOAT="float"
-	[ -z "$USE_DOUBLE" ] || unset USE_DOUBLE
+	[ -z "$USE_DOUBLE" ] && [ "$USE_DOUBLE" -eq 0 ] && unset USE_DOUBLE
     if [ "$SP_BRANCH" = "master" ]
     then
 		wget https://gist.githubusercontent.com/fennecdjay/a5dbc54342bcf6f0c8d5f9a03355580b/raw/f7a6a000b6687fb5edc494c2d5fe05e8bb30bc40/soundpipe_data_master.patch
