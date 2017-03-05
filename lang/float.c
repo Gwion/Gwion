33d9248aa350e3550ed713a28c7e9655374612b1
fennecdjay <astor.jeremie@wanadoo.fr>
2017-03-05 15:37:58 +0100

    Fix float assign.

--- lang/float.c
+++ lang/float.c
@@ -10,10 +10,10 @@ struct Type_ t_float     = { "float",      sizeof(m_float),  NULL,  te_float };
 static INSTR(assign)
 {
 #ifdef DEBUG_INSTR
-  debug_msg("instr", "(float) '=' %f %f", **(m_float**)(shred->reg - SZ_FLOAT * 2), *(m_float*)(shred->reg - SZ_FLOAT));
+  debug_msg("instr", "(float) '=' %f %f", **(m_float**)(shred->reg - SZ_FLOAT + SZ_INT), *(m_float*)(shred->reg - SZ_FLOAT));
 #endif
-  POP_REG(shred, SZ_FLOAT * 2);
-  *(m_float*)(shred->reg) = (**(m_float**)shred->reg = *(m_float*)(shred->reg + SZ_FLOAT));
+  POP_REG(shred, SZ_FLOAT + SZ_INT);
+  *(m_float*)(shred->reg) = (**(m_float**)shred->reg = *(m_float*)(shred->reg + SZ_INT));
   PUSH_REG(shred, SZ_FLOAT);
 }
 
