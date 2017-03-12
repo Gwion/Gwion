cf0866e7aa8c71b48239b0d0dc63889d0b6303c5
fennecdjay <astor.jeremie@wanadoo.fr>
2017-03-12 18:52:26 +0100

    Last fixes for now.

--- util/travis_prepare.sh
+++ util/travis_prepare.sh
@@ -25,34 +25,6 @@ brew_dependencies() {
 	brew install lua        # needed for importing soundpipe
 }
 
-install_bison() {
-	wget "http://ftp.gnu.org/gnu/bison/$BISON_VERSION.tar.gz"
-	tar -xzf "$BISON_VERSION.tar.gz"
-	pushd "$BISON_VERSION"
-    ./configure --prefix="$PWD/../bison"
-    make install
-    popd
-}
-
-install_soundpipe() {
-	git clone -b "$SP_BRANCH" https://github.com/paulbatchelor/Soundpipe.git
-#	git clone -b "$SP_BRANCH" https://github.com/fennecdjay/Soundpipe.git
-	pushd Soundpipe
-#	[ "$USE_DOUBLE" -eq 1 ] && export SP_FLOAT="double"
-	[ "$USE_DOUBLE" -eq 0 ] && export SPFLOAT="float"
-	[ -z "$USE_DOUBLE" ] && export SPFLOAT="float"
-	[ "$USE_DOUBLE" -eq 0 ] && unset USE_DOUBLE
-#    if [ "$SP_BRANCH" = "master" ]
-#    then
-#		wget https://gist.githubusercontent.com/fennecdjay/a5dbc54342bcf6f0c8d5f9a03355580b/raw/f7a6a000b6687fb5edc494c2d5fe05e8bb30bc40/soundpipe_data_master.patch
-#		patch -p1 -i soundpipe_data_master.patch
-#		rm soundpipe_data_master.patch
-#		rm modules/data/padsynth.lua
-# 	fi
-    make
-	popd
-}
-
 build_soundpipe() {
 	[ "$USE_DOUBLE" -eq 0 ] && export SPFLOAT="float"
 	[ -z "$USE_DOUBLE" ] && export SPFLOAT="float"
@@ -83,13 +55,8 @@ prepare_directories() {
 	mkdir -p add
 }
 
-[ "$#" = 1 ] && [ "$1" = "check_soundpipe" ] && check_soundpipe && return
-
-
 [ "$TRAVIS_OS_NAME" = "osx" ] && brew_dependencies
 
-#install_bison
-#install_soundpipe
 check_soundpipe
 prepare_directories
 exit 0
