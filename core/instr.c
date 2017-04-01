fc400e4f96ece8e235be67572ffd07634e4144ac
fennecdjay <astor.jeremie@wanadoo.fr>
2017-04-01 12:23:49 +0200

    Fix Reg_Push_Imm compiler warning.

--- core/instr.c
+++ core/instr.c
@@ -32,7 +32,7 @@ INSTR(Reg_Push_Imm)
 #ifdef DEBUG_INSTR
   debug_msg("instr", "[reg] push imm %p %i", (void*)instr->m_val, instr->m_val2);
 #endif
-  *(m_uint*)(shred->reg) = instr->m_val2 ? &instr->m_val : instr->m_val;
+  *(m_uint*)(shred->reg) = instr->m_val2 ? (m_uint)&instr->m_val : instr->m_val;
   PUSH_REG(shred,  SZ_INT);
 }
 
