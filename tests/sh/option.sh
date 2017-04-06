937f42fbfbd51eb84c2ac38d4db4090edaf4e9c2
fennecdjay <astor.jeremie@wanadoo.fr>
2017-04-06 14:15:10 +0200

    option test

--- tests/sh/option.sh
+++ tests/sh/option.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# [test] #2
+# [test] #4
 n=0
 [ "$1" ] && n="$1"
 [ "$n" -eq 0 ] && n=1
 source tests/sh/common.sh
 
-run "$((n))" "remove " "- 1" "file2"&
-run "$((n+1))" "add  " "+ examples/int.gw" "file1"
+run "$((n+3))" "remove " "-l1" "file1"&
+run "$((n))" "remove " "- 1" "file2"
+run "$((n+1))" "add  " "+ examples/int.gw -l1" "file3"
+run "$((n+2))" "add  " "-l0" "file4"
+wait
