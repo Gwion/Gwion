b26adf21f2ebf5e46c5e564c9a719602357957e8
fennecdjay <astor.jeremie@wanadoo.fr>
2017-04-24 12:52:00 +0200

    Fix.

--- util/travis_prepare.sh
+++ util/travis_prepare.sh
@@ -32,6 +32,7 @@ build_soundpipe() {
 
 check_soundpipe() {
 ls
+rm -rf Soundpipe
 	[ -d Soundpipe ] || {
 		git clone -b "$SP_BRANCH" https://github.com/paulbatchelor/Soundpipe.git
 		pushd Soundpipe
