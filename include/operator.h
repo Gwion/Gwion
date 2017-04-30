12b43abe8bf8022c066b9f9da1d79b0492e807c9
fennecdjay <astor.jeremie@wanadoo.fr>
2017-04-30 19:33:36 +0200

    Update header.

--- include/operator.h
+++ include/operator.h
@@ -3,7 +3,7 @@
 typedef void (*f_instr)(VM* vm, VM_Shred shred, Instr instr);
 
 m_str op2str(Operator op);
-m_bool add_binary_op(Env env, Operator op, Type lhs, Type rhs, Type ret, f_instr f, m_bool global, m_bool is_new);
+m_bool add_binary_op(Env env, Operator op, Type lhs, Type rhs, Type ret, f_instr f, m_bool global);
 Type get_return_type(Env env, Operator op, Type rhs, Type lhs);
 //Operator name2op(m_str);
 m_int name2op(m_str);
