8f30ec27cfc693951d4addc6b5abd26988bb9c15
fennecdjay <astor.jeremie@wanadoo.fr>
2017-05-01 17:55:29 +0200

    Use signal.

--- tests/sh/udp.sh
+++ tests/sh/udp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # [test] #7
 n=0
+set -m
 [ "$1" ] && n="$1"
 [ "$n" -eq 0 ] && n=1
 
@@ -15,5 +16,8 @@ run "$((n+1))" "add file" "examples/sine.gw" "file3"
 run "$((n+2))" "remove shred" '- 1' "file4"
 run "$((n+3))" "unset loop" "-l0" "file5"
 run "$((n+4))" "quit" "-q" "file6"
-run "$((n+6))" "quit" "-h invalid" "file7"
+wait
+run "$((n+6))" "quit" "-h invalid -l 1" "file7"&
+#sleep .1
+kill %1
 wait
