7ab671eac87f42ceac076ba737274fc05a7b0cb0
fennecdjay <astor.jeremie@wanadoo.fr>
2017-05-30 22:13:56 +0200

    Fix option test.

--- tests/sh/option.sh
+++ tests/sh/option.sh
@@ -5,11 +5,11 @@ n=0
 [ "$n" -eq 0 ] && n=1
 source tests/sh/common.sh
 
-run "$((n+3))" "remove " "-l1" "file1"&
-sleep .1
+run "$((n+3))" "start loop " "-l1" "file1"&
+sleep .3
 run "$((n))" "remove " "- 1" "file2"
 run "$((n+1))" "add  " "+ examples/int.gw -l1" "file3"
-run "$((n+2))" "add  " "-l0" "file4"
+run "$((n+2))" "end loop  " "-l0" "file4"
 wait
 
 n=$((n+4))
