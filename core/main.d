b96ffcf8bf7d57ee362160f65a7aef50ceff79b9
fennecdjay <astor.jeremie@wanadoo.fr>
2017-06-04 20:05:20 +0200

    clean.

--- core/main.d
+++
@@ -1,9 +0,0 @@
-main.o: core/main.c include/err_msg.h include/defs.h include/generated.h \
- include/map.h include/defs.h include/symbol.h include/vm.h include/map.h \
- include/lang.h include/object.h include/array.h include/emit.h \
- include/type.h include/symbol.h include/nspc.h include/vm.h \
- include/operator.h include/oo.h include/env.h include/absyn.h \
- include/value.h include/dl.h include/frame.h include/absyn.h \
- include/emit.h include/compile.h include/udp.h include/driver.h \
- include/shreduler.h include/instr.h include/func.h include/lang.h \
- include/shreduler.h include/import.h include/bbq.h core/code_private.h
