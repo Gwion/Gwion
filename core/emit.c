dbbeb909bb0c0691d09390e05196551bf6496904
fennecdjay <astor.jeremie@wanadoo.fr>
2017-06-16 14:33:03 +0200

    Try emit (2)

--- core/emit.c
+++ core/emit.c
@@ -1700,8 +1700,9 @@ static m_bool emit_func_def(Emitter emit, Func_Def func_def) {
   emit->env->func = func;
   vector_add(emit->stack, (vtype)emit->code);
   emit->code = new_code();
+  char c[256]:
   char c[(emit->env->class_def ? strlen(emit->env->class_def->name) + 1 : 0) + strlen(func->name) + 6];
-  sprintf(c, "%s%s%s(...)", emit->env->class_def ? emit->env->class_def->name : "", emit->env->class_def ? "." : " ", func->name);
+  sprintf(c, "%s%s%s(...)", emit->env->class_def ? emit->env->class_def->name : "", emit->env->class_def ? "." : "", func->name);
   emit->code->name = strdup(c);
   emit->code->need_this = func->is_member;
   emit->code->filename = strdup(emit->filename);
