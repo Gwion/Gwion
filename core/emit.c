529e35ddaa6b3449e7e8dff1136d0c2125591cf3
fennecdjay <astor.jeremie@wanadoo.fr>
2017-06-16 12:27:44 +0200

    Revert emit

--- core/emit.c
+++ core/emit.c
@@ -1700,8 +1700,7 @@ static m_bool emit_func_def(Emitter emit, Func_Def func_def) {
   emit->env->func = func;
   vector_add(emit->stack, (vtype)emit->code);
   emit->code = new_code();
-  char c[strlen(func->name) +
-	(emit->env->class_def ? (strlen(emit->env->class_def->name) + 8) : 6)];
+  char c[256];
   sprintf(c, "%s%s%s(...)", emit->env->class_def ? emit->env->class_def->name : "", emit->env->class_def ? "." : " ", func->name);
   emit->code->name = strdup(c);
   emit->code->need_this = func->is_member;
@@ -1802,7 +1801,7 @@ static m_bool emit_class_def(Emitter emit, Class_Def class_def) {
   emit->env->class_def = type;
   vector_add(emit->stack, (vtype)emit->code);
   emit->code = new_code();
-  char c[strlen(type->name) + 7];
+  char c[256];
   sprintf(c, "class %s", type->name);
   emit->code->name = strdup(c);
 
