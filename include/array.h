97353a13be091ea030c6e029fbfcb722888c9488
fennecdjay <astor.jeremie@wanadoo.fr>
2017-07-24 00:38:30 +0200

    :shirt: Clean array include

--- include/array.h
+++ include/array.h
@@ -1,29 +1,29 @@
 typedef struct {
+  Type   type;
   m_uint length;
   m_uint depth;
-  Type type;
   m_bool is_obj;
   m_bool is_ref;
 } VM_Array_Info;
 
-m_uint   m_vector_size(M_Vector v);
+m_uint     m_vector_size(M_Vector v);
 
-m_uint   i_vector_at(M_Vector v, m_uint i);
-m_uint*  i_vector_addr(M_Vector v, m_uint i);
-void     i_vector_set(M_Vector v, m_uint i, m_uint  data);
+m_uint     i_vector_at(M_Vector v, m_uint i);
+m_uint*    i_vector_addr(M_Vector v, m_uint i);
+void       i_vector_set(M_Vector v, m_uint i, m_uint  data);
 
-m_float  f_vector_at(M_Vector v, m_uint i);
-m_float* f_vector_addr(M_Vector v, m_uint i);
-void     f_vector_set(M_Vector v, m_uint i, m_float data);
+m_float    f_vector_at(M_Vector v, m_uint i);
+m_float*   f_vector_addr(M_Vector v, m_uint i);
+void       f_vector_set(M_Vector v, m_uint i, m_float data);
 
 m_complex  c_vector_at(M_Vector v, m_uint i);
 m_complex* c_vector_addr(M_Vector v, m_uint i);
 void       c_vector_set(M_Vector v, m_uint i, m_complex data);
 
-m_vec3   v3_vector_at(M_Vector v, m_uint i);
-m_vec3*  v3_vector_addr(M_Vector v, m_uint i);
-void     v3_vector_set(M_Vector v, m_uint i, m_vec3  data);
+m_vec3     v3_vector_at(M_Vector v, m_uint i);
+m_vec3*    v3_vector_addr(M_Vector v, m_uint i);
+void       v3_vector_set(M_Vector v, m_uint i, m_vec3  data);
 
-m_vec4   v4_vector_at(M_Vector v, m_uint i);
-m_vec4*  v4_vector_addr(M_Vector v, m_uint i);
-void     v4_vector_set(M_Vector v, m_uint i, m_vec4  data);
+m_vec4     v4_vector_at(M_Vector v, m_uint i);
+m_vec4*    v4_vector_addr(M_Vector v, m_uint i);
+void       v4_vector_set(M_Vector v, m_uint i, m_vec4  data);
