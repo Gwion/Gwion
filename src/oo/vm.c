ebff7304348a29f289004fb75d00a844d4f265a9
fennecdjay <astor.jeremie@wanadoo.fr>
2017-08-29 16:14:08 +0200

    :art: free_vm_code

--- src/oo/vm.c
+++ src/oo/vm.c
@@ -24,34 +24,41 @@ VM_Code new_vm_code(Vector instr, m_uint stack_depth, m_bool need_this,
   return code;
 }
 
-void free_vm_code(VM_Code a) {
+
+static void free_code_instr_gack(Instr instr) {
+  m_uint j;
+  Vector v = *(Vector*)instr->ptr;
+  for(j = vector_size(v) + 1; --j;)
+    REM_REF(((Type)vector_at(v, j - 1)));
+  free_vector(v);
+}
+
+static void free_code_instr(Vector v) {
   m_uint i;
+  for(i = vector_size(v) + 1; --i;) {
+    Instr instr = (Instr)vector_at(v, i - 1);
+    if(instr->execute == Instr_Array_Init ||
+        instr->execute == Instr_Array_Alloc)
+      free(*(VM_Array_Info**)instr->ptr);
+    else if(instr->execute == Gack)
+      free_code_instr_gack(instr);
+    else if(instr->execute == Branch_Switch)
+      free_map(*(Map*)instr->ptr);
+    else if(instr->execute == Spork && instr->m_val2)
+        REM_REF(((Func)instr->m_val2))
+    else if(instr->execute == Init_Loop_Counter)
+      free((m_int*)instr->m_val);
+    free(instr);
+  } 
+  free_vector(v);
+}
+
+void free_vm_code(VM_Code a) {
   if(!strcmp(a->name, "[dtor]")) { // dtor from release. free only EOC
     free((void*)vector_back(a->instr));
     free_vector(a->instr);
-  } else if(a->instr) {
-    for(i = vector_size(a->instr) + 1; --i;) {
-      Instr instr = (Instr)vector_at(a->instr, i - 1);
-      if(instr->execute == Instr_Array_Init ||
-          instr->execute == Instr_Array_Alloc)
-        free(*(VM_Array_Info**)instr->ptr);
-      else if(instr->execute == Gack) {
-        m_uint j;
-        Vector v = *(Vector*)instr->ptr;
-        for(j = vector_size(v) + 1; --j;)
-          REM_REF(((Type)vector_at(v, j - 1)));
-        free_vector(v);
-      } else if(instr->execute == Branch_Switch)
-        free_map(*(Map*)instr->ptr);
-      else if(instr->execute == Spork) {
-        if(instr->m_val2)
-          REM_REF(((Func)instr->m_val2))
-        } else if(instr->execute == Init_Loop_Counter)
-        free((m_int*)instr->m_val);
-      free(instr);
-    }
-    free_vector(a->instr);
-  }
+  } else if(a->instr)
+    free_code_instr(a->instr);
   free(a->name);
   free(a->filename);
   free(a);
