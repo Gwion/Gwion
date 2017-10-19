ada19a93a1db659ea0d9d5e1ade06229c82b2995
fennecdjay <astor.jeremie@wanadoo.fr>
2017-10-19 12:48:48 +0200

    :bomb: Prevent tempate class_def recreation

--- src/parse/scan.c
+++ src/parse/scan.c
@@ -232,7 +232,8 @@ m_bool scan1_exp_decl(Env env, Exp_Decl* decl) {
       if(!list->self->array->exp_list)
         SET_FLAG(decl->type, ae_flag_ref);
     }
-    list->self->value = new_value(t, s_name(list->self->xid));
+    if(!list->self->value)
+      list->self->value = new_value(t, s_name(list->self->xid));
     if(GET_FLAG(decl->type, ae_flag_const)) {
       SET_FLAG(list->self->value, ae_flag_const);
       SET_FLAG(list->self->value, ae_flag_uconst);
