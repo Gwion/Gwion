acd78e241a8c99909944158cc99cb434b8382df1
fennecdjay <astor.jeremie@wanadoo.fr>
2017-10-30 16:50:05 +0100

    :santa: Update plugins

--- tests/test_plugins/array.c
+++ tests/test_plugins/array.c
@@ -10,14 +10,12 @@ MFUN(test_mfun){}
 IMPORT
 {
   CHECK_BB(importer_class_ini(importer, &t_invalid_var_name, NULL, NULL))
-
-	importer_item_ini(importer,"int[]", "int_array");
-  importer_item_end(importer, 0, NULL); // import array var
-  importer_func_ini(importer, "float[][]", "f", (m_uint)test_mfun);
+  CHECK_BB(importer_item_ini(importer,"int[]", "int_array"))
+  CHECK_BB(importer_item_end(importer, 0, NULL)) // import array var
+  CHECK_BB(importer_func_ini(importer, "float[][]", "f", (m_uint)test_mfun))
   CHECK_BB(importer_func_end(importer, 0))
-  importer_func_ini(importer, "float[][]", "g", (m_uint)test_mfun);
+  CHECK_BB(importer_func_ini(importer, "float[][]", "g", (m_uint)test_mfun))
   CHECK_BB(importer_func_end(importer, 0))
-
   CHECK_BB(importer_class_end(importer))
   return 1;
 }
--- tests/test_plugins/op_err.c
+++ tests/test_plugins/op_err.c
@@ -9,7 +9,7 @@ static struct Type_ t_invalid_var_name = { "invalid_var_name", SZ_INT, &t_object
 MFUN(test_mfun){}
 IMPORT
 {
-  importer_oper_ini(importer, "int", "int", "int"); // ignore the check.
+  importer_oper_ini(importer, "int", "int", "int"); // ignore the check
   importer_oper_end(importer, 220, NULL, 1); // ignore the check.
   CHECK_BB(importer_oper_ini(importer, "int", "int", "int"))
   CHECK_BB(importer_oper_end(importer, op_chuck, NULL, 1))
