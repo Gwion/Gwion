05965858ddf211405fa8c4b0a0d9970fa3e12e1d
fennecdjay <astor.jeremie@wanadoo.fr>
2017-11-01 19:26:34 +0100

    :art: function pointer in plugins: add some tests.

---
+++ tests/test_plugins/typedef.c
@@ -0,0 +1,22 @@
+#include "type.h"
+#include "import.h"
+#include "object.h"
+#include "emit.h"
+
+static struct Type_ t_func_typedef = { "FuncTypedef", SZ_INT, &t_object };
+
+static MFUN(test_func) { puts("test"); }
+IMPORT {
+  CHECK_BB(importer_fptr_ini(importer, "void", "PtrType"))
+  CHECK_BB(importer_fptr_end(importer, 0))
+  
+  CHECK_BB(importer_class_ini(importer, &t_func_typedef, NULL, NULL))
+    CHECK_BB(importer_fptr_ini(importer, "void", "PtrType"))
+    CHECK_BB(importer_fptr_end(importer, ae_flag_static))
+    CHECK_BB(importer_func_ini(importer, "void", "test_func", test_func))
+    CHECK_BB(importer_func_end(importer, ae_flag_static))
+    CHECK_BB(importer_item_ini(importer, "PtrType", "ptr"))
+    CHECK_BB(importer_item_end(importer, ae_flag_static, NULL))
+  CHECK_BB(importer_class_end(importer))
+  return 1;
+}
