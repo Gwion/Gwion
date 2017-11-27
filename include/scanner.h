ecfb14532cb05d1c0af1d68ed0ca53e77a930051
fennecdjay <astor.jeremie@wanadoo.fr>
2017-11-27 05:41:39 +0100

    :art: Disable block comment

--- include/scanner.h
+++ include/scanner.h
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include "map.h"
 
 typedef struct {
   void* scanner;
   Ast ast;
   unsigned int line;
   unsigned int pos;
-  unsigned int comment_depth;
   FILE* file;
   m_str filename;
 } Scanner;
