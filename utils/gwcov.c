e57c9f00b31be3a831ea34ac26aa977aee7209fb
fennecdjay <astor.jeremie@wanadoo.fr>
2017-12-19 18:05:12 +0100

    :art: start to  modularize gwcov

--- utils/gwcov.c
+++ utils/gwcov.c
@@ -10,13 +10,28 @@
 #define TABLEN 2
 #define MIN_LINE 64
 typedef struct {
-  unsigned int set;
-  unsigned int ini;
-  /*unsigned int end;*/
+  uint set;
+  uint ini;
 } Line;
 
-static int last, max_exec;
-static Line* lines;
+typedef struct {
+  uint line_count;
+  char* line;
+  uint i;
+  char c[8];
+  ssize_t s;
+} Data;
+
+typedef struct {
+  const char* base;
+  FILE* out;
+  uint last;
+  uint max_exec;
+  Line* lines;
+  char* postfix;
+} Cov;
+
+typedef void (*cov_func)(Cov*,Data*);
 
 static void err(const char* base, const char* file) {
   fprintf(stderr, "Unable to do coverage for '\033[1m%s\033[0m'.\n", base);
@@ -28,64 +43,36 @@ static void err(const char* base, const char* file) {
 
 }
 
-static void da(char* base) {
-  int line_count = MIN_LINE;
-  int curr_line;
-  char c[8];
-  char filename[strlen(base) + 3];
-  sprintf(filename, "%sda", base);
-  FILE* f = fopen(filename, "r");
-
-  char * line = NULL;
-  if(!f) // err msg
-    err(base, filename);
-  while (1) {
-    int ret = fscanf(f, "%i %s", &curr_line, (char*)&c);
-    if(ret == 2) {
-      if(curr_line >= line_count) {
-        lines = realloc(lines, 2 * line_count * sizeof(Line));
-        memset(lines + line_count, 0, line_count * sizeof(Line));
-        line_count *= 2;
-      }
-      lines[curr_line].set++;
-      if(curr_line > last)
-        last = curr_line;
-    }
-    else if(errno != 0) {
-      perror("scanf:");
-      break;
-    }
-    else if(ret == EOF) {
-        break;
-    } else {
-        fprintf(stderr, "No match.\n");
-    }
+static void da(Cov* cov, Data* d) {
+  if(d->i >= d->line_count) {
+    cov->lines = realloc(cov->lines, 2 * d->line_count * sizeof(Line));
+    memset(cov->lines + d->line_count, 0, d->line_count * sizeof(Line));
+    d->line_count *= 2;
   }
-  fclose(f);
-  if(line)
-    free(line);
+  cov->lines[d->i].set++;
+  if(d->i > cov->last)
+    cov->last = d->i;
 }
 
-void cov(char* base) {
-  char filename[strlen(base) + 4];
-  int curr_line;
-  char c[8];
+static void co(Cov *cov, Data* d) {
+  if(!strcmp(d->c, "ini")) {
+    cov->lines[d->i].ini++;
+    cov->max_exec++;
+  }
+}
+
+static void run(Cov *cov, void(*func)(Cov*, Data*)) {
+  char filename[strlen(cov->base) + strlen(cov->postfix)+ 1];
+  Data d = { MIN_LINE };
   FILE* f;
 
-  sprintf(filename, "%scov", base);
-  f = fopen(filename, "r");
-  if(!f) // err msg
-    err(base, filename);
+  sprintf(filename, "%s%s", cov->base, cov->postfix);
+  if(!(f = fopen(filename, "r")))
+    err(cov->base, filename);
   while (1) {
-    int ret = fscanf(f, "%i %s", &curr_line, (char*)&c);
-    if(ret == 2) {
-      if(!strcmp(c, "ini")) {
-        lines[curr_line].ini++;
-        max_exec++;
-      }
-      /*else*/
-      /*lines[curr_line].end++;*/
-    }
+    int ret = fscanf(f, "%u %s", &d.i, (char*)&d.c);
+    if(ret == 2)
+      func(cov, &d);
     else if(errno != 0) {
       perror("scanf:");
       break;
@@ -125,72 +112,87 @@ static void detab(char* in, char* out, size_t max_len, uint* has_comment) {
   out[i] = 0;
 }
 
-static void fill(uint min, uint max) {
+static void fill(Cov* cov, uint min, uint max) {
   while(min++ < max)
-    fprintf(stdout, " ");
+    fprintf(cov->out, " ");
 }
 
-void diagnostic(char* filename){
-  FILE * f;
-  char * line = NULL;
-  size_t len = 0;
-  ssize_t read;
-  int line_size, line_count = 1;
-  int max_line_digit = last ? floor(log10(last) + 1) : 1;
-  int max_exec_digit = last ? floor(log10(max_exec) + 1) : 1;
+static void tty(Cov* cov, Data* d) {
+  int max_line_digit = floor(log10(cov->last) + 1);
+  int max_exec_digit = floor(log10(cov->max_exec) + 1);
   struct winsize w;
 
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-  line_size = w.ws_col - max_line_digit - max_exec_digit - 6;
+  uint line_size = w.ws_col - max_line_digit - max_exec_digit - 6;
+  uint too_long = 0;
+  uint has_comment = 0;
+  int num_digit = d->line_count ? floor(log10(d->line_count) + 1) : 1;
+  ssize_t line_len = (d->s > line_size) ? line_size  - 1: d->s - 1;
+  if(d->s > line_size)
+    too_long = 1;
+  const char* prefix;
+  char detabed[TABLEN*line_len];
+  char *stripped_line = d->s == 1 ? strdup("") : strndup(d->line, line_len);
+  detab(stripped_line, detabed, TABLEN*line_len, &has_comment);
+  free(stripped_line);
+  line_len = strlen(detabed);
+  if(cov->lines[d->line_count].set) {
+    prefix = cov->lines[d->line_count].ini ?
+    "\033[32m" : "\033[31m";
+  } else
+    prefix = "";
+  fprintf(cov->out, "\033[2m%i", d->line_count);
+  fill(cov, num_digit, max_line_digit);
+  fprintf(cov->out, ":\033[0m %s%s\033[0m\033[2m", prefix, detabed);
+  fill(cov, line_len, line_size + (has_comment ? 8 : 0));
+  if(cov->lines[d->line_count].set)
+    fprintf(cov->out, "%s (%i)\n\033[0m",
+        too_long ? "\b\b..." : "|", cov->lines[d->line_count].ini);
+  else
+    puts("| ...\033[0m");
+}
 
-  if(!(f = fopen(filename, "r")))
-    err(filename, filename);
+static void terminal(Cov* cov, Data* d) {
+  if(isatty(fileno(cov->out)))
+    tty(cov, d);
+  else
+    fprintf(cov->out, "%i\n", cov->lines[d->line_count].ini);
+}
 
-  while((read = getline(&line, &len, f)) != -1) {
-    uint too_long = 0;
-    uint has_comment = 0;
-    int num_digit = line_count ? floor(log10(line_count) + 1) : 1;
-    ssize_t line_len = read > line_size ? line_size : read - 1;
-    if(read >= line_size)
-      too_long = 1;
-    const char* prefix;
-    char detabed[TABLEN*line_len];
-    char *stripped_line = read == 1 ? strdup("") : strndup(line, line_len);
-    detab(stripped_line, detabed, TABLEN*line_len, &has_comment);
-    free(stripped_line);
-    line_len = strlen(detabed);
-    if(lines[line_count].set) {
-      prefix = lines[line_count].ini ?
-      "\033[32m" : "\033[31m";
-    } else
-      prefix = "";
-    fprintf(stdout, "\033[2m%i", line_count);
-    fill(num_digit, max_line_digit);
-    fprintf(stdout, ":\033[0m %s%s\033[0m\033[2m", prefix, detabed);
-    fill(line_len, line_size + (has_comment ? 8 : 0));
-    if(lines[line_count].set)
-      fprintf(stdout, "%s (%i)\n\033[0m",
-          too_long ? "\b\b..." : "|", lines[line_count].ini);
-    else
-      puts("| ...\033[0m");
-    line_count++;
+void diagnostic(Cov* cov, cov_func func){
+  FILE * f;
+  Data d = { 1, NULL };
+  size_t len = 0;
+
+  if(!(f = fopen(cov->base, "r")))
+    err(cov->base, cov->base);
+  while((d.s = getline(&d.line, &len, f)) != -1) {
+    func(cov, &d);
+    d.line_count++;
   }
   fclose(f);
-  if(line)
-    free(line);
+  if(d.line)
+    free(d.line);
 }
 
 int main(int argc, char** argv) {
+  int max_exec = 0;
+  cov_func func = terminal;
+  FILE* out = stdout;
   argv++;
   argc--;
   while(argc) {
-    lines = calloc(MIN_LINE, sizeof(Line));
-    da(*argv);
-    cov(*argv);
-    diagnostic(*argv);
-    free(lines);
+    Cov c = { *argv, out, 1, max_exec };
+    c.lines = calloc(MIN_LINE, sizeof(Line));
+    c. postfix = "da";
+    run(&c, da);
+    c. postfix = "cov";
+    run(&c, co);
+    diagnostic(&c, func);
+    free(c.lines);
     argc--;
     argv++;
+    max_exec = c.max_exec;
   }
   exit(EXIT_SUCCESS);
 }
