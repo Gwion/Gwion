bf488280abe720d778699f1a9af2edb19be01c19
fennecdjay <astor.jeremie@wanadoo.fr>
2018-01-14 21:19:00 +0100

    :bug: Fix Exception printing

--- src/lib/object.c
+++ src/lib/object.c
@@ -8,7 +8,7 @@ struct Type_ t_null    = { "@null",     SZ_INT, NULL,      te_null};
 struct Type_ t_object  = { "Object",    SZ_INT, NULL,      te_object };
 
 void NullException(VM_Shred shred, const m_str c) {
-  err_msg(INSTR_, 0, "%s: shred[id=%" UINT_F ":%s], PC=[%" UINT_F "]\n",
+  err_msg(INSTR_, 0, "%s: shred[id=%" UINT_F ":%s], PC=[%" UINT_F "]",
           c, shred->xid, shred->name, shred->pc);
   release(shred->me, shred);
   shred->me = NULL;
