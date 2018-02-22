4d277e2b3116bb70d745efd28bb0de4838d4a488
fennecdjay <astor.jeremie@wanadoo.fr>
2018-02-23 00:00:46 +0100

    :art: No More static stuff in Sndfile driver

--- src/drvr/sndfile.c
+++ src/drvr/sndfile.c
@@ -8,51 +8,45 @@
 #include "vm.h"
 #include "driver.h"
 
-static SNDFILE** sf;
-static m_uint nchan;
-static m_uint bufsize;
-VM* vm;
-
-static m_bool sndfile_ini(VM* v, DriverInfo* di) {
-  nchan = di->chan;
-  bufsize = di->bufsize;
-  sf = calloc(nchan, sizeof(void*));
+static m_bool sndfile_ini(VM* vm, DriverInfo* di) {
+  SNDFILE** sf = calloc(di->chan, sizeof(void*));
   char tmp[140];
   SF_INFO info;
-  vm = v;
   info.samplerate = di->sr;
   info.channels = 1;
   info.format = SF_FORMAT_WAV | SF_FORMAT_PCM_24;
   m_uint chan;
-  if(nchan == 1) {
+  if(di->chan == 1) {
     sprintf(tmp, "%s.wav", di->card);
     sf[0] = sf_open(tmp, SFM_WRITE, &info);
-  } else for(chan = 0; chan < nchan; chan++) {
+  } else for(chan = 0; chan < di->chan; chan++) {
       sprintf(tmp, "%s_%02" UINT_F ".wav", di->card, chan);
       sf[chan] = sf_open(tmp, SFM_WRITE, &info);
-    }
+  }
+  di->data = sf;
   return 1;
 }
 
 static void sndfile_run(VM* vm, DriverInfo* di) {
   m_uint i, chan;
   sp_data* sp = vm->sp;
-  m_float buf[nchan][bufsize];
+  SNDFILE** sf = (SNDFILE**)di->data;
+  m_float buf[di->chan][di->bufsize];
   while(vm->is_running) {
-    for(i = 0; i < bufsize; i++) {
+    for(i = 0; i < di->bufsize; i++) {
       di->run(vm);
-      for(chan = 0; chan < nchan; chan++)
+      for(chan = 0; chan < di->chan; chan++)
         buf[chan][i] = sp->out[chan];
       sp->pos++;
     }
-    for(chan = 0; chan < nchan; chan++)
-      sf_write(sf[chan], (const m_float*)buf[chan], bufsize);
+    for(chan = 0; chan < di->chan; chan++)
+      sf_write(sf[chan], (const m_float*)buf[chan], di->bufsize);
   }
 }
 
 static void sndfile_del(VM* vm, DriverInfo* di) {
-  m_uint i;
-  for(i = 0; i < nchan; i++)
+  SNDFILE** sf = (SNDFILE**)di->data;
+  for(m_uint i = 0; i < di->chan; i++)
     sf_close(sf[i]);
   free(sf);
 }
