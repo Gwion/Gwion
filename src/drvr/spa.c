cd3bdbd83d65345f5ee8ce47479b48aa91889bf4
fennecdjay <astor.jeremie@wanadoo.fr>
2018-02-22 23:51:06 +0100

    :art: No More static stuff in Spa driver

--- src/drvr/spa.c
+++ src/drvr/spa.c
@@ -3,29 +3,31 @@
 #include "vm.h"
 #include "driver.h"
 
-static sp_audio spa;
-
 static m_bool spa_ini(VM* vm, DriverInfo* di) {
+  di->data = malloc(sizeof(sp_audio));
   return 1;
 }
 
 static void spa_run(VM* vm, DriverInfo* di) {
   char tmp[strlen(di->card) + 5];
   sp_data* sp = vm->sp;
+  sp_audio* spa = di->data;
   sprintf(tmp, "%s.spa", di->card);
-  if(spa_open(vm->sp, &spa, tmp, SPA_WRITE) == SP_NOT_OK) {
+  if(spa_open(vm->sp, spa, tmp, SPA_WRITE) == SP_NOT_OK) {
     fprintf(stderr, "Error: could not open file %s.\n", di->card);
     return;
   }
   while(vm->is_running) {
     di->run(vm);
-    spa_write_buf(vm->sp, &spa, sp->out, sp->nchan);
+    spa_write_buf(vm->sp, spa, sp->out, sp->nchan);
     sp->pos++;
   }
 }
 
 static void spa_del(VM* vm, DriverInfo* di) {
-  spa_close(&spa);
+  sp_audio* spa = (sp_audio*)di->data;
+  spa_close(spa);
+  free(spa);
 }
 
 void spa_driver(Driver* d) {
