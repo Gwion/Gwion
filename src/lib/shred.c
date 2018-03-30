87cd11ef0ee5d73cce8c2bb8a66c6941a3795851
fennecdjay <astor.jeremie@wanadoo.fr>
2018-03-31 01:10:43 +0200

    :art: Fix shred_from_id

--- src/lib/shred.c
+++ src/lib/shred.c
@@ -40,13 +40,16 @@ static MFUN(shred_yield) {
 }
 
 static SFUN(vm_shred_from_id) {
-  const VM_Shred s = (VM_Shred)vector_at(&shred->vm_ref->shred, *(m_uint*)MEM(SZ_INT) - 1);
-  if(s) {
-    *(m_uint*)RETURN = (m_uint)s->me;
-    s->me->ref++;
-    vector_add(&shred->gc, (vtype) s->me);
-  } else
-    *(m_uint*)RETURN = 0;
+  const m_int index =  *(m_uint*)MEM(SZ_INT);
+  *(m_uint*)RETURN = 0;
+  if(index > 0) {
+    const VM_Shred s = (VM_Shred)vector_at(&shred->vm_ref->shred, index);
+    if(s) {
+      *(m_uint*)RETURN = (m_uint)s->me;
+      s->me->ref++;
+      vector_add(&shred->gc, (vtype) s->me);
+    }
+  }
 }
 
 static MFUN(shred_args) {
