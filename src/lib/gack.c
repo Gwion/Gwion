f4d2bb50dcfb3e3cccce703b7ea35f26e6ab3c79
fennecdjay <astor.jeremie@wanadoo.fr>
2018-07-28 10:19:27 +0200

    :art: Simplify gack

--- src/lib/gack.c
+++ src/lib/gack.c
@@ -6,22 +6,10 @@
 #include "err_msg.h"
 #include "instr.h"
 
-#ifdef COLOR
-#define BOLD  "\033[1m"
-#define CLEAR "\033[0m"
-#define GREEN "\033[32m"
-#define BLUE  "\033[34m"
-#else
-#define BOLD  ""
-#define CLEAR ""
-#define GREEN ""
-#define BLUE  ""
-#endif
-
-static void print_type(Type type) {
+static void print_type(const Type type) {
   const m_bool is_func = isa(type, t_function) > 0 && isa(type, t_func_ptr) < 0;
   const m_str name = is_func ? strdup("@function") : strdup(type->name);
-  gw_out(BOLD "(" GREEN "%s" CLEAR BOLD ") " CLEAR, name);
+  gw_out("(%s) ", name);
   free(name);
   if(GET_FLAG(type, ae_flag_typedef)) {
     gw_out(" aka ");
@@ -29,15 +17,15 @@ static void print_type(Type type) {
   }
 }
 
-static void print_int(m_int i) {
-  gw_out(BOLD "%" INT_F "" CLEAR , i);
+static void print_int(const m_int i) {
+  gw_out("%" INT_F "", i);
 }
 
-static void print_float(m_float f) {
-  gw_out(BOLD "%.4f" CLEAR, f);
+static void print_float(const m_float f) {
+  gw_out("%.4f", f);
 }
 
-static void print_complex(m_complex c) {
+static void print_complex(const m_complex c) {
   gw_out("#(");
   print_float(creal(c));
   gw_out(", ");
@@ -45,7 +33,7 @@ static void print_complex(m_complex c) {
   gw_out(")");
 }
 
-static void print_polar(m_complex c) {
+static void print_polar(const m_complex c) {
   gw_out("%%(");
   print_float(creal(c));
   gw_out(", ");
@@ -53,10 +41,9 @@ static void print_polar(m_complex c) {
   gw_out("*pi)");
 }
 
-static void print_vec(m_bit* f, m_uint size) {
-  m_uint i;
+static void print_vec(const m_bit* f, const m_uint size) {
   gw_out("@(");
-  for(i = 0; i < size; i++) {
+  for(m_uint i = 0; i < size; i++) {
     print_float(creal(*(m_float*)(f + i * SZ_FLOAT)));
     if(i < size - 1)
       gw_out(", ");
@@ -64,41 +51,41 @@ static void print_vec(m_bit* f, m_uint size) {
   gw_out(")");
 }
 
-static void print_string1(m_str str) {
-  gw_out(BOLD "%s" CLEAR, str);
+static void print_string1(const m_str str) {
+  gw_out("%s", str);
 }
 
-static void print_string(M_Object obj) {
+static void print_string(const M_Object obj) {
   print_string1(obj ? STRING(obj) : "(null string)");
 }
 
-static void print_object(Type type, M_Object obj) {
+static void print_object(const Type type, const M_Object obj) {
   if(isa(type, t_string) > 0)
     print_string(obj);
   else
-    gw_out(BOLD "%p" CLEAR, (void*)obj);
+    gw_out("%p", (void*)obj);
 }
 
-static void print_func(Type type, m_bit* stack) {
-    if(isa(type, t_func_ptr) > 0)
-      gw_out(BOLD "%p" CLEAR, (void*) * (Func*)stack);
-    else
-      gw_out(BOLD "%s" CLEAR, type->name);
+static void print_func(const Type type, const m_bit* stack) {
+  if(isa(type, t_func_ptr) > 0)
+    gw_out("%p", (void*) * (Func*)stack);
+  else
+    gw_out("%s", type->name);
 }
 
-static void print_prim(Type type, m_bit* stack) {
-    if(isa(type, t_int) > 0)
-      print_int(*(m_int*)stack);
-    else if(isa(type, t_complex) > 0)
-      print_complex(*(m_complex*)stack);
-    else if(isa(type, t_polar) > 0)
-      print_polar(*(m_complex*)stack);
-    else if(isa(type, t_vec3) > 0)
-      print_vec(stack, 3);
-    else if(isa(type, t_vec4) > 0)
-      print_vec(stack, 4);
-    else
-     print_float(*(m_float*)stack);
+static void print_prim(const Type type, const m_bit* stack) {
+  if(isa(type, t_int) > 0)
+    print_int(*(m_int*)stack);
+  else if(isa(type, t_complex) > 0)
+    print_complex(*(m_complex*)stack);
+  else if(isa(type, t_polar) > 0)
+    print_polar(*(m_complex*)stack);
+  else if(isa(type, t_vec3) > 0)
+    print_vec(stack, 3);
+  else if(isa(type, t_vec4) > 0)
+    print_vec(stack, 4);
+  else
+   print_float(*(m_float*)stack);
 }
 
 INSTR(Gack) { GWDEBUG_EXE
@@ -107,7 +94,7 @@ INSTR(Gack) { GWDEBUG_EXE
   m_uint offset = instr->m_val;
   for(m_uint i = size + 1; --i;) {
     const Type type = (Type)vector_at(v, size - i);
-    if(size == 1 && isa(type, t_class) < 0)
+    if(size == 1)
       print_type(type);
     if(isa(type, t_object) > 0)
       print_object(type, *(M_Object*)REG(-offset));
@@ -121,5 +108,5 @@ INSTR(Gack) { GWDEBUG_EXE
       print_prim(type, REG(-offset));
     offset -= type->size;
   }
-  gw_out(CLEAR"\n");
+  gw_out("\n");
 }
