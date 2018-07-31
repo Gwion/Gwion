5feb5095c84c6a6da603ceb088a6ec6bf12ecb8b
fennecdjay <astor.jeremie@wanadoo.fr>
2018-07-31 20:03:03 +0200

    :art: Add hex escape sequence

--- src/emit/escape.c
+++ src/emit/escape.c
@@ -27,7 +27,7 @@ m_bool escape_str(m_str str_lit, const int pos) {
   m_str str = str_lit;
   while(*str_lit) {
     if(*str_lit == '\\')  {
-      str_lit++;
+      ++str_lit;
       if(*str_lit == '\0')
         CHECK_BB(err_msg(UTIL_, pos,
               "invalid: string ends with escape charactor '\\'"))
@@ -41,21 +41,26 @@ m_bool escape_str(m_str str_lit, const int pos) {
           if(c2 >= '0' && c2 <= '7' && c3 >= '0' && c3 <= '7') {
             *str++ = (c-'0') * 64 + (c2-'0') * 8 + (c3-'0');
             str_lit += 2;
-          }
-          else
+          } else
             CHECK_BB(err_msg(UTIL_, pos,
                   "malformed octal escape sequence '\\%c%c%c'", c, c2, c3))
         }
-      }
-      else if(c == 'x')
-        CHECK_BB(err_msg(UTIL_, pos,
-              "hex escape sequence not (yet) supported (use octal!)"))
-      else
+      } else if(c == 'x') {
+        ++str_lit;
+        const unsigned char c = *(str_lit);
+        const unsigned char c2 = *(str_lit+1);
+        if(c >= '0' && c <= 'F' && c2 >= '0' && c2 <= 'F') {
+          *str++ =(c-'0') * 16 + (c2-'0');
+          ++str_lit;
+        } else
+          CHECK_BB(err_msg(UTIL_, pos,
+                  "malformed hex escape sequence '\\%c%c'", c, c2))
+      } else
         CHECK_BB((*str++ = get_escape(c, pos)))
     }
     else
         *str++ = *str_lit;
-    str_lit++;
+    ++str_lit;
   }
   *str = '\0';
   return 1;
