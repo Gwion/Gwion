0004131770a968d6fd6fab49b90dd8dfdb387363
fennecdjay <astor.jeremie@wanadoo.fr>
2018-10-20 18:38:46 +0200

    :art: Improve type.c

--- src/oo/type.c
+++ src/oo/type.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "type.h"
 #include "mpool.h"
 
@@ -12,22 +11,9 @@ ANN2(2) Type new_type(const m_uint xid, const m_str name, const Type parent) {
 }
 
 ANN void free_type(Type a) {
-//  if(a->p) {
-//    mp_end(a->p);
-//    xfree(a->p);
-//  }
   if(a->nspc)
     REM_REF(a->nspc);
-  if(GET_FLAG(a, ae_flag_builtin)) {
-    if(a->parent && a->parent->array_depth)
-      REM_REF(a->parent)
-    if(GET_FLAG(a, ae_flag_template))
-      mp_free(Type, a);
-  } else {
-    if(GET_FLAG(a, ae_flag_typedef) && GET_FLAG(a->parent, ae_flag_typedef))
-      free_class_def(a->parent->def);
-    mp_free(Type, a);
-  }
+  mp_free(Type, a);
 }
 
 ANN Type type_copy(const Type type) {
@@ -35,7 +21,7 @@ ANN Type type_copy(const Type type) {
   a->nspc          = type->nspc;
   a->owner         = type->owner;
   a->size          = type->size;
-  a->d.base_type = type->d.base_type;
+  a->d.base_type   = type->d.base_type;
   a->array_depth   = type->array_depth;
   a->def           = type->def;
   return a;
