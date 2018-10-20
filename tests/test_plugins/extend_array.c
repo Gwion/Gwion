b10a99613e97b73c1b6ee8ad13cf7d63779ed4f9
fennecdjay <astor.jeremie@wanadoo.fr>
2018-10-20 22:20:59 +0200

    :art: Update tests

--- tests/test_plugins/extend_array.c
+++ tests/test_plugins/extend_array.c
@@ -11,7 +11,7 @@ GWION_IMPORT(extend_array_test) {
   CHECK_BB(gwi_class_ini(gwi, t_array_ext, NULL, NULL))
   Type_Decl* td = new_type_decl(new_id_list(insert_symbol("float"), 0), 0, 0);
   Exp e = new_exp_prim_int(1, 0);
-  Array_Sub array = new_array_sub(e, 0);
+  Array_Sub array = new_array_sub(e);
   add_type_decl_array(td, array);
   CHECK_BB(gwi_class_ext(gwi, td))
   CHECK_BB(gwi_class_end(gwi))
