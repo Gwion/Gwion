688c805695ee0ba58444f5e9baf377bb4ff62c25
fennecdjay <astor.jeremie@wanadoo.fr>
2018-11-01 14:06:54 +0100

    :art: err_msg2

--- include/err_msg.h
+++
@@ -1,8 +0,0 @@
-#include <stdarg.h>
-m_bool err_msg(m_uint pos, const char* fmt, ...);
-m_bool gw_err(const char* fmt, ...);
-m_bool gw_out(const char* fmt, ...);
-
-#define ERR_O(a,...) { err_msg(a, ## __VA_ARGS__); return NULL; }
-#define ERR_B(a,...) { err_msg(a, ## __VA_ARGS__); return   -1; }
-#define GWDEBUG_EXE
