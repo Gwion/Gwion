2344781719aa3529d07bb6e33d8a7bc332aca4a9
fennecdjay <astor.jeremie@wanadoo.fr>
2018-11-21 15:05:20 +0100

    :art: spork test

--- src/emit/emit.c
+++ src/emit/emit.c
@@ -761,7 +761,8 @@ ANN2(1,2) static m_bool emit_exp_spork_finish(const Emitter emit, const VM_Code 
   *(VM_Code*)push_code->ptr = code;
   const Instr spork = emitter_add_instr(emit, f ? SporkExp : SporkFunc);
   spork->m_val = f ? emit_code_offset(emit) : arg_size;
-  spork->m_val2 = f ? (m_uint)f : (m_uint)code;
+//  spork->m_val2 = f ? (m_uint)f : (m_uint)code;
+  spork->m_val2 = (m_uint)code;
   *(m_uint*)spork->ptr = depth;
   return 1;
 }
@@ -798,34 +799,39 @@ static m_bool scoped_stmt(const Emitter emit, const Stmt stmt, const m_bool pop)
   emit_pop_scope(emit);
   return 1;
 }
-
+/*
 ANN static Func make_spork_func(const Stmt stmt) {
   char c[12 + num_digit(stmt->pos)];
-  sprintf(c, "spork~func:%i", stmt->pos);
+  sprintf(c, "spork~func:%u", stmt->pos);
   const Symbol sporked = insert_symbol(c);
   const ID_List list = new_id_list(sporked, stmt->pos);
   const Func_Def def = new_func_def(new_type_decl(list, 0), sporked, NULL, stmt, 0);
   return new_func(c, def);
 }
-
+*/
 ANN static m_bool spork_func(const Emitter emit, const Stmt stmt) { GWDEBUG_EXE
   if(emit->env->class_def)
     push_this(emit);
-  const Func f = make_spork_func(stmt);
-  const Instr push = emitter_add_instr(emit, RegPushImm);
-  *(Func*)push->ptr = f;
-  emit_push_code(emit, f->name);
+//  const Func f = make_spork_func(stmt);
+//  const Instr push = 
+emitter_add_instr(emit, RegPushImm);
+//  *(Func*)push->ptr = f;
+//  emit_push_code(emit, f->name);
+  emit_push_code(emit, "sporked");
   if(emit->env->class_def) { // check we're not in static func ?
-    SET_FLAG(f, ae_flag_member);
+//    SET_FLAG(f, ae_flag_member);
     SET_FLAG(emit->code, ae_flag_member);
     emit_alloc_local(emit, SZ_INT, 0);
   }
   const Instr op = emitter_add_instr(emit, MemPushImm);
   op->m_val = emit->code->stack_depth;
   CHECK_BB(scoped_stmt(emit, stmt, 0))
-  f->code = finalyze(emit);
+//  f->code = finalyze(emit);
+  VM_Code code = finalyze(emit);
   emit_pop_code(emit);
-  return emit_exp_spork_finish(emit, f->code, f, 0, emit->env->func ?
+//  return emit_exp_spork_finish(emit, f->code, f, 0, emit->env->func ?
+//      emit->env->func->def->stack_depth : 0);
+  return emit_exp_spork_finish(emit, code, (Func)code, 0, emit->env->func ?
       emit->env->func->def->stack_depth : 0);
 }
 
