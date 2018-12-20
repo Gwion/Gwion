e96342ed68d96f89a95b26eba6b6b33ca75b513b
fennecdjay <astor.jeremie@wanadoo.fr>
2018-12-20 14:16:43 +0100

    :art: Use macro for constant_xxx

--- opt/constant.c
+++ opt/constant.c
@@ -4,14 +4,11 @@
 #include "type.h"
 #include "constant.h"
 
-ANN m_bool constant_int(const Exp e) {
-  return (e->exp_type == ae_exp_primary &&
-    e->d.exp_primary.primary_type == ae_primary_num) ||
-    (e->exp_type == ae_exp_constprop && isa(e->type , t_int) > 0);
+#define describe_constant_xxx(name, etype, mtype)                  \
+ANN m_bool constant_##name(const Exp e) {                          \
+  return (e->exp_type == ae_exp_primary &&                         \
+    e->d.exp_primary.primary_type == ae_primary_##etype) ||        \
+    (e->exp_type == ae_exp_constprop && isa(e->type , mtype) > 0); \
 }
-
-ANN m_bool constant_float(const Exp e) {
-  return (e->exp_type == ae_exp_primary &&
-    e->d.exp_primary.primary_type == ae_primary_float) ||
-    (e->exp_type == ae_exp_constprop && isa(e->type , t_float) > 0);
-}
+describe_constant_xxx(int,   num,  t_int)
+describe_constant_xxx(float, float, t_float)
