cb81b8af36423365f62f2e447991e5d89eda9a8b
fennecdjay <astor.jeremie@wanadoo.fr>
2019-02-16 13:08:47 +0100

    :art: finish removibg constprop

--- include/optim.h
+++ include/optim.h
@@ -3,7 +3,7 @@
 #ifdef OPTIMIZE
 #define OPTIMIZE_CONST(a) CHECK_BO(optimize_const(a))
 ANN m_bool optimize_const(const Exp_Binary*);
-ANN2(1) void constprop_prim(const Exp_Primary* p, m_uint* ptr);
+//ANN2(1) void constprop_prim(const Exp_Primary* p, m_uint* ptr);
 #else
 #endif
 m_bool constant_folding(const Exp_Binary* bin);
