2078cfb4a341dc5b0b3018a7fef4eba586bd920b
fennecdjay <astor.jeremie@wanadoo.fr>
2019-07-24 16:41:18 +0200

    :book: Update docs

--- help/struct_check.sh
+++ help/struct_check.sh
@@ -4,4 +4,4 @@
 pahole -s ${PRG} | while read -r name size gap
 do [ "$size" -gt 64 ] && echo "$name $size"
    [ "$gap" -gt 0 ] && echo "$name has a gap"
-done | grep -v IO_FILE | grep -v __jmp_buf_tag | grep -v yyguts_t
+done #| grep -v IO_FILE | grep -v __jmp_buf_tag | grep -v yyguts_t
