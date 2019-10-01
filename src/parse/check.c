2a185b3f7e3f20201dc98fec2f9b3408294aa3f5
fennecdjay <astor.jeremie@wanadoo.fr>
2019-10-01 21:53:56 +0200

    :art: try to use use check_implicit in is_int

--- src/parse/check.c
+++ src/parse/check.c
@@ -411,6 +411,9 @@ static inline m_bool index_is_int(const Env env, Exp e, m_uint *depth) {
 
   const m_str str = "@access";
   const Type t = env->gwion->type[et_int];
+//  do CHECK_BB(check_implicit(env, "@access", e, env->gwion->type[et_int]))
+  do CHECK_BB(check_implicit(env, str, e, t))
+/*
   do {
     struct Implicit imp = { .e=e, .t=t, .pos=e->pos };
     struct Op_Import opi = { .op=insert_symbol(str), .lhs=e->type,
@@ -418,6 +421,7 @@ static inline m_bool index_is_int(const Env env, Exp e, m_uint *depth) {
     CHECK_OB(op_check(env, &opi))
     e->nspc = env->curr;
   }
+*/
   while(++(*depth) && (e = e->next));
   return GW_OK;
 }
