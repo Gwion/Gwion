755cdbb1d3167411961dbf5364226ab2da3f3de9
fennecdjay <astor.jeremie@wanadoo.fr>
2019-10-08 02:10:30 +0200

    :white_check_mark: already imported op

---
+++ tests/import/op_already_imported.c
@@ -0,0 +1,21 @@
+#include "gwion_util.h"
+#include "gwion_ast.h"
+#include "oo.h"
+#include "vm.h"
+#include "env.h"
+#include "type.h"
+#include "instr.h"
+#include "object.h"
+#include "instr.h"
+#include "vm.h"
+#include "gwion.h"
+#include "operator.h"
+#include "import.h"
+#include "gwi.h"
+
+
+GWION_IMPORT(op_already_imported) {
+  CHECK_BB(gwi_oper_ini(gwi, "int", "int", "int"))
+  CHECK_BB(gwi_oper_end(gwi, "=>", NULL))
+  return GW_OK;
+}
