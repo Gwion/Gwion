fbf6736b29f74a9e6841448e5dc4152262b05793
fennecdjay <astor.jeremie@wanadoo.fr>
2019-10-11 15:56:55 +0200

    :art: Fix get_type_name

--- src/oo/type.c
+++ src/oo/type.c
@@ -171,7 +171,6 @@ ANN m_str get_type_name(const Env env, const m_str s, const m_uint index) {
   ++name;
   while(*name++) {
     if(*name == '<') {
-      c[i++] = *name;
       lvl++;
       name++;
     } else if(*name == '~' && !lvl--)
