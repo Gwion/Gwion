63abeb2cf483b262a096cfbeeeb931485908b2df
fennecdjay <astor.jeremie@wanadoo.fr>
2019-11-04 17:30:01 +0100

    :art: Update

---
+++ tests/import/func_fail4.c
@@ -0,0 +1,20 @@
+#include "gwion_util.h"
+#include "gwion_ast.h"
+#include "oo.h"
+#include "vm.h"
+#include "env.h"
+#include "type.h"
+#include "instr.h"
+#include "object.h"
+#include "gwion.h"
+#include "value.h"
+#include "operator.h"
+#include "import.h"
+#include "instr.h"
+
+GWION_IMPORT(too_many_args) {
+  GWI_BB(gwi_func_ini(gwi, "Ptr<~int~>", "test<~A~>"))
+  GWI_BB(gwi_func_arg(gwi, "int", "i[][]"))
+  GWI_BB(gwi_item_end(gwi, ae_flag_none, NULL))
+  return GW_OK;
+}
