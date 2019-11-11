1e0d32c5d233d9a8cb87510d6f4ed7965d063219
fennecdjay <astor.jeremie@wanadoo.fr>
2019-11-11 12:23:21 +0100

    :art: Simplify

--- scripts/ensure.sh
+++ scripts/ensure.sh
@@ -1,46 +1,4 @@
 #!/bin/sh
-toml() {
-cat <<EOF > book.toml
-[book]
-authors = ["Jérémie Astor"]
-title = "Gwion"
-
-[output.html]
-default-theme = "rust"
-additional-css = ["custom.css"]
-
-[output.html.fold]
-enable = true
-level = 0
-EOF
-}
-
-css() {
-cat <<EOF > custom.css
-.mdr {
-  color:var(--sidebar-active);
-  background-color:var(--sidebar-bg);
-  border: 5px solid var(--sidebar-fg);
-  padding: 10px;
-  margin-right: 20%;
-  margin-left: 20%;
-  moz-border-radius: 15px;
-  -webkit-border-radius: 15px;
-}
-
-footer {
-  padding: 10px;
-  text-align: center;
-  font-size: small;
-  font-style: italic;
-}
-EOF
-}
-
-[ -f book.toml ] || toml
-
-[ -f custom.css ] || css
-
 [ -d src ] || sh scripts/mdr2mdbook.sh
 
 [ -f src/SUMMARY.md ] || sh scripts/summary.sh > src/SUMMARY.md
