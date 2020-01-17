18640a33dad267bc3b76225e55ff95f556008f70
Jérémie Astor <astor.jeremie@wanadoo.fr>
2020-01-17 01:12:41 +0100

    allow pref

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -10,8 +10,7 @@ run() {
   echo "$1 $3 $2" >&2
   echo "perf stat -r$repeats $1 $test_dir/$3.$2"
 #  sudo echo -1 > /proc/sys/kernel/perf_event_paranoid
-  sudo perf stat $1 $test_dir/$3.$2 >&2
-  perf stat -r"$repeats" "$1" "$test_dir/$3.$2" 2>&1 | grep "time elapsed" |
+  sudo perf stat -r"$repeats" "$1" "$test_dir/$3.$2" 2>&1 | grep "time elapsed" |
     sed 's/ *\([0-9]*\),\([0-9]*\) .* seconds time elapsed *( +- *\([0-9]*\),\([0-9]*\)% )/\1.\2 \3.\4/'
 }
 
