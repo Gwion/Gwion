8a260e93f46c4d8f83e23a5769566f1680e89709
Jérémie Astor <astor.jeremie@wanadoo.fr>
2020-01-20 00:11:47 +0100

    escape

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-language=("gwion" "wren" "lua" "python" "chuck" "ruby" "perl")
-extension=("gw" "wren" "lua" "py" "ck" "rb" "pl")
+language=("gwion" "wren" "lua" "python" "chuck" "ruby")
+extension=("gw" "wren" "lua" "py" "ck" "rb")
 test_dir="benchmark"
 plot_script="scripts/bench.plot"
 : "${repeats:=10}"
