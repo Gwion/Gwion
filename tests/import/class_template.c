c8f58c2f79c1714d337b57447265fc0c10fc455d
Jérémie Astor <astor.jeremie@wanadoo.fr>
2020-05-04 23:39:19 +0200

    :white_check_mark: Update plug tests

--- tests/import/class_template.c
+++ tests/import/class_template.c
@@ -25,17 +25,17 @@ static CTOR(class_template_ctor) {
 }
 
 GWION_IMPORT(class_template) {
-  GWI_OB(gwi_class_ini(gwi, "ClassTemplate<~A,B~>", NULL))
+  GWI_OB(gwi_class_ini(gwi, "<~A,B~>ClassTemplate", NULL))
   gwi_class_xtor(gwi, class_template_ctor, NULL);
     GWI_BB(gwi_item_ini(gwi, "A[]", "key"))
     GWI_BB((o_map_key = gwi_item_end(gwi, ae_flag_member | ae_flag_template, NULL)))
     GWI_BB(gwi_item_ini(gwi, "B[]", "value"))
     GWI_BB((o_map_value = gwi_item_end(gwi, ae_flag_member, NULL)))
-   GWI_BB(gwi_func_ini(gwi, "int", "test<~C,D~>"))
+   GWI_BB(gwi_func_ini(gwi, "int", "<~C,D~>test"))
    GWI_BB(gwi_func_end(gwi, (f_xfun)1, ae_flag_none))
 
   GWI_BB(gwi_class_end(gwi))
-  GWI_BB(gwi_item_ini(gwi, "ClassTemplate<~Ptr<~int~>,int[]~>", "testObject"))
+  GWI_BB(gwi_item_ini(gwi, "<~Ptr<~int~>,int[]~>ClassTemplate", "testObject"))
   GWI_BB(gwi_item_end(gwi, ae_flag_none, NULL))
   return GW_OK;
 }
--- tests/import/func_fail4.c
+++ tests/import/func_fail4.c
@@ -10,7 +10,7 @@
 #include "instr.h"
 
 GWION_IMPORT(too_many_args) {
-  GWI_BB(gwi_func_ini(gwi, "Ptr<~int~>", "test<~A~>"))
+  GWI_BB(gwi_func_ini(gwi, "<~int~>Ptr", "<~A~>test"))
   GWI_BB(gwi_func_arg(gwi, "int", "i[][]"))
   GWI_BB(gwi_item_end(gwi, ae_flag_none, NULL))
   return GW_OK;
