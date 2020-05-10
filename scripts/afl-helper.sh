33e4b6c6b9d990901fb09e539f17fbcafcff5de6
Jérémie Astor <astor.jeremie@wanadoo.fr>
2020-05-10 17:52:24 +0200

    :wrench: Update afl-hepler

--- scripts/afl-helper.sh
+++ scripts/afl-helper.sh
@@ -1,11 +1,24 @@
-set -e
 [ $# -lt 2 ] && {
   echo "usage: afl-helper crash_dir out_dir"
   exit 1
 }
 
+# check false positive
+FUZZER=$PWD/gwion-fuzz
+FAULT=0
+pushd $1
+for a in id*
+do
+  cat $a | $FUZZER || FAULT=$((FAULT+1))
+done
+[ $FAULT = 0 ] && { echo "only false positive"; exit 1; }
+popd
+
+set -e
+
 DEST=$(realpath $2)
-cd $DEST
+[ -d $DEST ] || mkdir $DEST
+pushd $DEST
 NUM=$(ls *.gw | wc -l)
 TMP=0
 for a in *.gw
@@ -13,10 +26,12 @@ do
   mv $a ${TMP}.gw
   TMP=$((TMP+1))
 done
+popd
 
-cd $1
+pushd $1
 for a in id*
 do
   cp $a $DEST/${NUM}.gw
   NUM=$((NUM+1))
 done
+popd
