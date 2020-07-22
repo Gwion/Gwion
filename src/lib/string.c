dfe1ec48c45e75bcc885a3ff921b4fae013c7912
Jérémie Astor <astor.jeremie@wanadoo.fr>
2020-07-22 21:31:31 +0200

    :art: String formatting

--- src/lib/string.c
+++ src/lib/string.c
@@ -465,7 +465,7 @@ GWION_IMPORT(string) {
 
   gwi_item_ini(gwi, "@internal", "@data");
   GWI_BB(gwi_item_end(gwi,   ae_flag_const, NULL))
-  
+
   gwi_func_ini(gwi, "int", "size");
   GWI_BB(gwi_func_end(gwi, string_len, ae_flag_none))
 
