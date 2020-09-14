c18a8e5768fe94a085f5a3b4280c7a7596e7b7d2
Jérémie Astor <astor.jeremie@wanadoo.fr>
2020-09-14 11:32:33 +0200

    :wrench Use asan

--- scripts/test.sh
+++ scripts/test.sh
@@ -398,15 +398,7 @@ clean() {
 
 if [ -f test.log ] && [ -z "$NO_LOG" ]
 then
-#  cat test.log
-# assuming CI for now
-  sudo apt-get update -y
-  sudo apt-get install -y valgrind
-  for file in $(cat test.log)
-  do
-      cat $file
-      valgrind ./gwion $file
-  done
+  cat test.log
   exit 1
 else
   exit 0
