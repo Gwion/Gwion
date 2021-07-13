2540c3d39786403e11828233b68bc2fa5a76c62f
GitHub Action <action@github.com>
2021-07-13 13:01:47 +0000

    add book

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-git checkout -b doc_tmp
-git branch -r | grep gh-pages && git push origin :gh-pages || true
-git add -f book
-git commit -m "add book"
-git subtree push --prefix book origin gh-pages
-git checkout mdbook
-git branch -D doc_tmp
