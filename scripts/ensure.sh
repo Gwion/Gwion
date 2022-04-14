522a3f7f5153d6ea94b6a493a963b48beb8cd0fd
Jérémie Astor <fennecdjay@gmail.com>
2022-04-14 15:28:18 +0200

    try using GwionSite

--- scripts/ensure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for file in $(find docs -type f -name "*.mdr")
-do mdr $file
-done
-
-[ -d src ] || bash scripts/mdr2mdbook.sh
-
-[ -f src/SUMMARY.md ] || bash scripts/summary.sh > src/SUMMARY.md
--- scripts/list_benchmarks.sh
+++
@@ -1,6 +0,0 @@
-for a in $(find gwion-benchmark/png -type f -name "*.png")
-do
-  png_url="https://raw.githubusercontent.com/Gwion/gwion-benchmark/results/png/$(basename $a)"
-  test_url="https://github.com/Gwion/gwion-benchmark/blob/master/src/$(basename $a .png).gw"
-  echo "<a href=\"$test_url\"><img class=\"zoom\" src=\"$png_url\" width=\"352px\"></a>"
-done
