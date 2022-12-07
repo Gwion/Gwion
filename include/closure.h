9b79ade8e8f0f2d5c66a0d8a8a2f9b966f53282b
fennecdjay <fennecdjay@gmail.com>
2022-12-07 14:39:40 +0100

    :art: update envset_pushv

---
+++ include/closure.h
@@ -0,0 +1,4 @@
+#ifndef __GWION_CLOSURE
+#define __GWION_CLOSURE
+ANN m_bool tmpl_fptr(const Env env, const Fptr_Def fptr, const Func_Def fdef);
+#endif
--- include/env/envset.h
+++ include/env/envset.h
@@ -16,11 +16,9 @@ struct EnvSet {
 ANN m_bool envset_run(struct EnvSet *, const Type);
 ANN2(1, 3) m_bool envset_push(struct EnvSet *, const Type, const Nspc);
 ANN static inline m_bool envset_pushv(struct EnvSet *es, const Value v) {
-//  es->_ctx         = es->env->context;
-//  es->_filename    = es->env->name;
+  es->_ctx         = es->env->context;
+  es->_filename    = es->env->name;
   CHECK_BB(envset_push(es, v->from->owner_class, v->from->owner));
-//  es->env->context = v->from->ctx;
-//  es->env->name    = v->from->filename;
   return GW_OK;
 }
 ANN2(1) void envset_pop(struct EnvSet *, const Type);
