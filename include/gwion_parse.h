b3f0de35b6f36a3efb8858a2f0485e12dece99c7
fennecdjay <fennecdjay@gmail.com>
2024-05-07 11:42:02 +0200

    :art: add missing header

---
+++ include/gwion_parse.h
@@ -0,0 +1,7 @@
+#pragma once
+ANN Ast gwion_parse_ast(const Gwion gwion, const char *data);
+ANN Func_Def gwion_parse_func_def(const Gwion gwion, const char *data, const loc_t loc);
+ANN Stmt_List gwion_parse_stmt_list(const Gwion gwion, const char *data, const loc_t loc);
+ANN Exp *gwion_parse_expression(const Gwion gwion, const char *data, const loc_t loc);
+ANN Type_Decl *gwion_parse_type_decl(const Gwion gwion, const char *data, const loc_t loc);
+
